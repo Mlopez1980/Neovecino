3cd1b3066abe02206f16f1a29f2c749d9c363032
Mlopez1980 <marco.lopez@hconstructores.com>
2026-05-05 08:47:18 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import NeoVecinoMVP from "./App.jsx";
+import ReactDOM from "react-dom/client";
 import "./index.css";
+import NeoVecinoMVP from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <NeoVecinoMVP />
   </React.StrictMode>
